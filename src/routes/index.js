d32b6baab63b935ed6f5a4c5d5a447769b9d2552
soijeongg <010127js@gmail.com>
2024-03-13 19:26:16 +0900

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,15 @@
 import express from 'express';
-import userRouter from "./user/user.router.js"
+// import cardRoutes from './card/card.router.js';
+import columnRouter from './column/column.router.js';
+import commentRouter from './comment/comment.router.js';
+import userRouter from './user/user.router.js';
 
-const router  = express.Router()
-router.use("/", userRouter)
+const router = express.Router();
+
+router.use('/boards/:boardId/columns', columnRouter);
+// router.use('/boards/:boardId/columns/:columnId/cards', cardRoutes);
+
+router.use('/cards/:cardld/comments', commentRouter);
+router.use('/', userRouter);
+
 export default router;
