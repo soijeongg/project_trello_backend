501b99748be5f6b654b783b1bd18cafd26b5e36b
jovid_18 <cshcho99@gmail.com>
2024-03-13 19:24:33 +0900

    FEAT: adjust card.joi.js to card.controller.js

--- src/routes/card/card.controller.js
+++ src/routes/card/card.controller.js
@@ -1,8 +1,18 @@
 import { CardService } from './card.service.js';
-
+import {
+  columnIdScehma,
+  createCardSchema,
+  updateCardSchema,
+} from './card.joi.js';
 export class CardController {
   getCards = async (req, res, next) => {
     try {
+      const { error } = columnIdScehma.validate(req.parans);
+      if (error) {
+        const error = new Error('주소 형식이 올바르지 않습니다.');
+        error.status = 400;
+        throw error;
+      }
       const { columnId } = req.params;
       const cards = await CardService.findAllCardWithColumnId(columnId);
       res.status(200).json(cards);
@@ -12,6 +22,18 @@ export class CardController {
   };
   createCard = async (req, res, next) => {
     try {
+      const { paramserror } = columnIdScehma.validate(req.parans);
+      if (error) {
+        const error = new Error('주소 형식이 올바르지 않습니다.');
+        error.status = 400;
+        throw error;
+      }
+      const { error } = createCardSchema.validate(req.body);
+      if (error) {
+        const error = new Error('요청 형식이 올바르지 않습니다.');
+        error.status = 400;
+        throw error;
+      }
       const { columnId } = req.params;
       const cardData = req.body;
       await CardService.createCard(columnId, cardData);
@@ -22,6 +44,12 @@ export class CardController {
   };
   updateCard = async (req, res, next) => {
     try {
+      const { error } = columnIdScehma.validate(req.parans);
+      if (error) {
+        const error = new Error('주소 형식이 올바르지 않습니다.');
+        error.status = 400;
+        throw error;
+      }
       const { cardId } = req.params;
       const cardData = req.body;
       await CardService.updateCard(cardId, cardData);
@@ -32,6 +60,12 @@ export class CardController {
   };
   deleteCard = async (req, res, next) => {
     try {
+      const { error } = columnIdScehma.validate(req.parans);
+      if (error) {
+        const error = new Error('주소 형식이 올바르지 않습니다.');
+        error.status = 400;
+        throw error;
+      }
       const { cardId } = req.params;
       await CardService.deleteCard(cardId);
       res.status(200).json({ message: '성공적으로 카드가 삭제되었습니다.' });
