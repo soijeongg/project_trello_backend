18b6e062bab15843b84536715f95fc1c71755428
jovid_18 <cshcho99@gmail.com>
2024-03-14 13:23:52 +0900

    FIX: fix card.controller.js for DI pattern

--- src/routes/card/card.controller.js
+++ src/routes/card/card.controller.js
@@ -12,7 +12,7 @@ export class CardsController {
         throw error;
       }
       const { columnId } = req.params;
-      const cards = await CardsService.findAllCardWithColumnId(columnId);
+      const cards = await this.CardsService.findAllCardWithColumnId(columnId);
       res.status(200).json(cards);
     } catch (err) {
       next(err);
@@ -37,7 +37,7 @@ export class CardsController {
       const { columnId } = req.params;
       const cardData = req.body;
       const cardWriterId = res.locals.user.userId;
-      await CardsService.createCard(columnId, cardWriterId, cardData);
+      await this.CardsService.createCard(columnId, cardWriterId, cardData);
       res.status(201).json({ message: '성공적으로 카드가 생성되었습니다.' });
     } catch (err) {
       next(err);
@@ -60,7 +60,7 @@ export class CardsController {
       const { cardId } = req.params;
       const cardData = req.body;
       const cardWriterId = res.locals.user.userId;
-      await CardsService.updateCard(cardId, cardWriterId, cardData);
+      await this.CardsService.updateCard(cardId, cardWriterId, cardData);
       res.status(200).json({ message: '성공적으로 카드가 수정되었습니다.' });
     } catch (err) {
       next(err);
@@ -75,7 +75,7 @@ export class CardsController {
         throw error;
       }
       const { cardId } = req.params;
-      await CardsService.deleteCard(cardId);
+      await this.CardsService.deleteCard(cardId);
       res.status(200).json({ message: '성공적으로 카드가 삭제되었습니다.' });
     } catch (err) {
       next(err);
