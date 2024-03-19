55f82e20fe4d572ba36af37794686d3d619507cc
jovid_18 <cshcho99@gmail.com>
2024-03-19 11:06:19 +0900

    FEAT: update multer package and s3

--- src/app.js
+++ src/app.js
@@ -49,7 +49,7 @@ app.use(
 );
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.urlencoded({ extended: false })); // url-encoded 형식의 데이터를 파싱할 수 있도록 미들웨어를 추가. extended: false 옵션은 Node.js의 기본 쿼리 문자열 파서를 사용하여 URL-encoded 데이터를 파싱
+app.use(express.urlencoded({ extended: true })); // url-encoded 형식의 데이터를 파싱할 수 있도록 미들웨어를 추가. extended: false 옵션은 Node.js의 기본 쿼리 문자열 파서를 사용하여 URL-encoded 데이터를 파싱
 app.get('/', (req, res) => {
   res.send('<h1>Trello</h1>');
 });
--- src/routes/board/board.router.js
+++ src/routes/board/board.router.js
@@ -4,19 +4,38 @@ import { BoardRepository } from './board.repository.js';
 import { BoardService } from './board.service.js';
 import { BoardController } from './board.controller.js';
 import authMiddleware from '../../middlewares/authMiddleware.js';
-
+import { S3Client } from '@aws-sdk/client-s3';
+import multer from 'multer';
+import multerS3 from 'multer-s3';
 const boardRepository = new BoardRepository(prisma);
 const boardService = new BoardService(boardRepository);
 const boardController = new BoardController(boardService);
 
 const router = express.Router();
+const s3 = new S3Client({
+  credentials: {
+    accessKeyId: process.env.S3_ACCESS_KEY,
+    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+  },
+  region: 'ap-northeast-2',
+});
+const upload = multer({
+  storage: multerS3({
+    s3,
+    bucket: 'nodejstrello',
+    key(req, file, cb) {
+      cb(null, `original/${Date.now()}_${file.originalname}`);
+    },
+  }),
+  limits: { fileSize: 5 * 1024 * 1024 },
+});
 
-router.post('/joinPage', authMiddleware,boardController.joinBoard); //authMiddleware, 
-router.get('/', authMiddleware, boardController.getBoards); //authMiddleware, 
+router.post('/joinPage', authMiddleware, boardController.joinBoard); //authMiddleware,
+router.get('/', authMiddleware, boardController.getBoards); //authMiddleware,
 router.get('/:boardId', authMiddleware, boardController.getBoardsId); //authMiddleware,
-router.post('/', authMiddleware,boardController.createBoard); //authMiddleware, 
-router.put('/:boardId',authMiddleware, boardController.updateBoard); //authMiddleware, 
-router.delete('/:boardId', authMiddleware, boardController.deleteBoard); //authMiddleware, 
+router.post('/', authMiddleware, upload.single('boardThumbnail'), boardController.createBoard); //authMiddleware,
+router.put('/:boardId', authMiddleware, boardController.updateBoard); //authMiddleware,
+router.delete('/:boardId', authMiddleware, boardController.deleteBoard); //authMiddleware,
 //user>UserBoard 접근
 router.get('/userBoard', authMiddleware, boardController.findUserBoard);
 //board>UserBoard 접근
