3e0f1fb5b5949f41d017c91d1703f8fd372f2016
nyeongsik13 <139778217+nyeongsik13@users.noreply.github.com>
2024-03-20 21:53:36 +0900

    FEAT: add socket.io room function

--- src/app.js
+++ src/app.js
@@ -61,14 +61,7 @@ app.use(
     credentials: true, // 쿠키를 포함한 요청을 허용
   })
 );
-// app.use(
-//   cors(
-//   //   {
-//   //   origin: true, // 요청이 온 도메인을 허용
-//   //   credentials: true, // 쿠키를 포함한 요청을 허용
-//   // }
-//   )
-// );
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true })); // url-encoded 형식의 데이터를 파싱할 수 있도록 미들웨어를 추가. extended: false 옵션은 Node.js의 기본 쿼리 문자열 파서를 사용하여 URL-encoded 데이터를 파싱
@@ -92,34 +85,58 @@ io.use(
 );
 
 io.on('connection', async (socket) => {
-  // async 키워드 추가
   console.log('연결 성공!');
-  const userId = socket.handshake.session.passport?.user; // Optional Chaining 사용
-
-  let userNickname = '익명'; // 기본값 설정
+  const userId = socket.handshake.session.passport?.user;
+  let userNickname = '익명';
+  let currentRoom = '';
 
   if (userId) {
     try {
-      const user = await prisma.user.findFirst({ where: { userId: +userId } }); // await 키워드 추가, findFirst -> findUnique 변경
-      userNickname = user?.nickname ?? '익명'; // user가 null이거나 nickname이 undefined일 경우를 대비한 처리
+      const user = await prisma.user.findUnique({ where: { userId: +userId } });
+      userNickname = user?.nickname ?? '익명';
     } catch (error) {
       console.error('사용자 정보 조회 중 오류 발생:', error);
     }
   }
 
-  // 클라이언트로부터 채팅 메시지를 받았을 때
-  socket.on('chat message', (msg) => {
-    // 이제 userNickname 변수는 채팅 메시지 이벤트 핸들러에서도 접근 가능
-    // console.log(`${userNickname}: ${msg.text}`);
+  // 전체 채팅에 입장
+  socket.join('main');
 
-    io.emit('chat message', { msg: msg.text, user: userNickname, own: socket.id });
+  // 방 입장 처리
+  socket.on('join room', (roomName) => {
+    if (currentRoom) {
+      socket.leave(currentRoom); // 이전 방에서 나감
+    }
+    socket.join(roomName); // 새로운 방에 입장
+    currentRoom = roomName;
+    console.log(`${userNickname} entered ${roomName}`);
   });
 
+  // 방 나가기 처리
+  socket.on('leave room', () => {
+    if (currentRoom) {
+      socket.leave(currentRoom); // 현재 방에서 나감
+      console.log(`${userNickname} left ${currentRoom}`);
+    }
+    socket.join('main'); // 기본 방으로 돌아감
+    currentRoom = 'main';
+  });
+
+  // 채팅 메시지 처리
+  socket.on('chat message', (msg) => {
+    const roomToEmit = currentRoom || 'main'; // 현재 방이 없으면 기본 방으로 설정
+    io.to(roomToEmit).emit('chat message', { msg: msg.text, user: userNickname, own: socket.id });
+    console.log(`Message in ${roomToEmit}: ${msg.text}`);
+  });
+
+  // 사용자 연결 해제 처리
   socket.on('disconnect', () => {
     console.log('User disconnected');
+    if (currentRoom) {
+      socket.leave(currentRoom); // 연결 해제 시 현재 방에서 나감
+    }
   });
 });
-
 httpServer.listen(PORT, () => {
   console.log(PORT, '포트로 서버가 열렸어요!');
 });
